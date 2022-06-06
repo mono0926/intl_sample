3f02be04c8c9b0b521b7f4ed81f40b59133b88a3
mono <mono0926@gmail.com>
2022-06-06 17:15:32 +0900

    Add Intl.getCurrentLocale log

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(const App());
 
@@ -23,6 +24,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final l10n = L10n.of(context);
+    print('current locale: ${Intl.getCurrentLocale()}');
     return Scaffold(
       appBar: AppBar(
         title: const Text('Intl Demo'),
